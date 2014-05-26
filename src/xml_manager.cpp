e994d5f580e97b1cdf33e441d7e385631ca26bd1
Alessio Balsini <a.balsini@sssup.it>
2014-05-26 18:04:58 +0200

    Added XML fixed task autoloader

--- src/xml_manager.cpp
+++ src/xml_manager.cpp
@@ -1,8 +1,39 @@
 #include "xml_manager.hpp"
 
+#include <string>
+
+#include "controller_global.hpp"
 #include "util.hpp"
+#include "pugixml/src/pugixml.hpp"
 
 void xml_parse(const std::string &path)
 {
   log("parsing file", path);
+
+  pugi::xml_document doc;
+  pugi::xml_parse_result result = doc.load_file(path.c_str());
+
+  pugi::xml_node SchedulingAlgorithm = doc.child("SchedulingAlgorithm");
+
+  std::cout
+      << "Load result: " << result.description() << std::endl
+      << "SchedulingAlgorithm name: " << SchedulingAlgorithm.attribute("name").value() << std::endl
+      << "path: " << SchedulingAlgorithm.child_value("path") << std::endl
+      << "args: " << SchedulingAlgorithm.child_value("args") << std::endl
+      << "period: " << SchedulingAlgorithm.child_value("period") << std::endl
+      << "deadline: " << SchedulingAlgorithm.child_value("deadline") << std::endl
+      << "runtime: " << SchedulingAlgorithm.child_value("runtime") << std::endl
+      << std::endl;
+
+  std::string executablePath;
+  std::string args;
+
+  executablePath = SchedulingAlgorithm.child_value("path");
+  args = SchedulingAlgorithm.child_value("args");
+
+  controller_global_launch(executablePath,
+                           args,
+                           atoi(SchedulingAlgorithm.child_value("runtime")),
+                           atoi(SchedulingAlgorithm.child_value("deadline")),
+                           atoi(SchedulingAlgorithm.child_value("period")));
 }
